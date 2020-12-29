b4b8421e83ae002df7ad5e6a02f99778f0ee518e
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2020-12-28 19:01:40 -0800

    v1.4.2 - Attempt at fixing rare game crash at launch

--- src/main/java/com/johnpyp/speedruntimer/SpeedrunTimer.java
+++ src/main/java/com/johnpyp/speedruntimer/SpeedrunTimer.java
@@ -9,6 +9,8 @@ import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @Environment(EnvType.CLIENT)
 public class SpeedrunTimer implements ModInitializer {
@@ -22,8 +24,26 @@ public class SpeedrunTimer implements ModInitializer {
         config.loadConfig();
         MinecraftClient client = MinecraftClient.getInstance();
         File configDir = FabricLoader.getInstance().getConfigDir().toFile();
-        DataStorage store = DataStorage.of(new File(configDir, "speedrun-timer.data.json"));
-        store.refreshBests("");
+        DataStorage store;
+        File file = new File(configDir, "speedrun-timer.data.json");
+        try {
+            store = DataStorage.of(file);
+            store.refreshBests("");
+        } catch (NullPointerException e) {
+            System.err.println("Deleting data file (speedrun-timer.data.json) because there was an error. File data:");
+            try {
+                System.err.println(Files.readString(Path.of(file.getPath())));
+            } catch (Exception ignored) {
+                System.err.println("Failed to print file data.");
+            }
+            try {
+                file.delete();
+            } catch (Exception ignored) {
+                System.err.println("Failed to delete data file (speedrun-timer.data.json).");
+            }
+            store = DataStorage.of(file);
+            store.refreshBests("");
+        }
         TickHandler tickHandler = new TickHandler(client, store, config);
         HudRenderCallback.EVENT.register((__, ___) -> tickHandler.tick());
     }
